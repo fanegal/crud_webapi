2762b353acdf5a0a874e99afdb216d43749170ca
Francisco Emmanuel Arteaga Ayala <00264480@red.unid.mx>
2021-12-22 00:06:54 -0600

    beta_2

--- crud_webapi/Startup.cs
+++ crud_webapi/Startup.cs
@@ -51,6 +51,11 @@ namespace crud_webapi
 
             services.AddAuthorization();
             services.AddSingleton<IJwtAuthenticationService>(new JwtAuthenticationService(key));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "crud_webapi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +64,8 @@ namespace crud_webapi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "crud_webapi v1"));
             }
 
             app.UseHttpsRedirection();
